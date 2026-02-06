f9bddef25e9a78e07d4500437a2a85322c85bf58
alishkaduz <duzelkhanaliakbar@gmail.com>
2026-02-06 19:57:18 +0500

    If-Else-Elif

---
+++ Practice 2/conditions/shorthand.py
@@ -0,0 +1,15 @@
+#shorthand if is used when you only have 1 line to execute
+a = 5
+b = 2
+if a > b: print("a is greater than b")
+
+#shorthand if else used when you have 1 line to execute for if and else
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+
+#multiple conditions in one line
+a = 330
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
---
+++ Practice 2/conditions/shrthnd_ex.py
@@ -0,0 +1,8 @@
+a = 10
+b = 20
+bigger = a if a > b else b
+print("Bigger is", bigger)
+
+username = ""
+display_name = username if username else "Guest"
+print("Welcome,", display_name)
