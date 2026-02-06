baa4dc3ac4476951ec7f4f7125b05217cd66a04a
alishkaduz <duzelkhanaliakbar@gmail.com>
2026-02-06 19:48:22 +0500

    booleans examples

---
+++ Practice 2/booleans/bools.py
@@ -0,0 +1,14 @@
+#bool true
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+
+
+#bool false
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
---
+++ Practice 2/booleans/comparison.py
@@ -0,0 +1,34 @@
+a = 200
+b = 33
+
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+
+
+
+#boolean as comparison
+x = 5
+y = 3
+
+print(x == y)
+print(x != y)
+print(x > y)
+print(x < y)
+print(x >= y)
+print(x <= y)
+
+x = 5
+
+print(1 < x < 10)
+
+print(1 < x and x < 10)
+
+
+
+x = 5
+
+print(not(x > 3 and x < 10))
