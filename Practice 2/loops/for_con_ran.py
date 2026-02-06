e88b5b5669b429e9bc1851c83fe9e38804f3290b
alishkaduz <duzelkhanaliakbar@gmail.com>
2026-02-06 20:32:39 +0500

    loops in python

---
+++ Practice 2/loops/for_con_ran.py
@@ -0,0 +1,19 @@
+# for continue
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    continue
+  print(x)  
+
+
+# for range
+for x in range(6):
+  print(x)
+
+
+for x in range(2, 6):
+  print(x)
+
+for x in range(2, 30, 3):
+  print(x)
---
+++ Practice 2/loops/while.py
@@ -0,0 +1,7 @@
+i = 1
+while i < 6:
+  print(i)
+  i += 1
+
+
+#With the while loop we can execute a set of statements as long as a condition is true
